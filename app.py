484da6e8c967f34a23246e22836d14970993221a
sandeepsuresh-hub <sandeepsuresh998@gmail.com>
2020-07-24 21:15:51 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def dashboard():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = ''
+	error = None
 	if request.method == 'POST':
-		if request.form['username' != 'admin' or request.form['password'] != 'admin']:
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
 			error = 'Invalid credentials. Please try again!!!'
 		else:
 			return redirect(url_for('home'))
